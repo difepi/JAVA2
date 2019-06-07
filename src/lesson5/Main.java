1f3f3910db5c9a1677146ebaf225792ece66324a
difepi <df@mymedtech.ru>
2019-06-07 16:56:10 +0300

    Домашнее задание 5. Параллелизм. Закон Амдала. Добавил ожидание заверш. побочных потоков. Join()

--- src/lesson5/Main.java
+++ src/lesson5/Main.java
@@ -1,8 +1,6 @@
 package lesson5;
 
-import java.util.Arrays;
-
-public  class Main {
+public class Main {
     static final int size = 10_000_000;
     static final int h = size / 2;
 
@@ -11,62 +9,73 @@ public  class Main {
         float[] arr = new float[size];
 
         noMulti(arr);
-        multi(arr);
+        try {
+            multi(arr);
 
-    }
-
-
-   private static void noMulti (float[] arr) {
-        for (int i = 0; i < size ; i++) {
-            arr[i]=1;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        long a=System.currentTimeMillis();
-
-       for (int i = 0; i < size ; i++) {
-           arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-
-       }
-       System.out.println("Выполнение в одном потоке " + Double.toString(System.currentTimeMillis()-a));
 
     }
 
 
-    private static void multi (float[] arr ) {
-        for (int i = 0; i < size ; i++) {
-            arr[i]=1;
+    private static void noMulti(float[] arr) {
+        for (int i = 0; i < size; i++) {
+            arr[i] = 1;
+        }
+        long a = System.currentTimeMillis();
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+
+        }
+        System.out.println("Выполнение в одном потоке " + Double.toString(System.currentTimeMillis() - a));
+
+    }
+
+
+    private static void multi(float[] arr) throws InterruptedException {
+        for (int i = 0; i < size; i++) {
+            arr[i] = 1;
         }
         float[] onearr = new float[h];
         float[] twoarr = new float[h];
 
-        long a=System.currentTimeMillis();
-        System.arraycopy(arr,0,onearr,0,h);
-        System.arraycopy(arr,h,twoarr,0,h);
+        long a = System.currentTimeMillis();
+        System.arraycopy(arr, 0, onearr, 0, h);
+        System.arraycopy(arr, h, twoarr, 0, h);
 
         Thread myThreadyOne = new Thread(new Runnable() {
             @Override
             public void run() {
                 for (int i = 0; i < h; i++) {
-                    onearr[i] = (float)(onearr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                    onearr[i] = (float) (onearr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
 
                 }
             }
         });
+
         Thread myThreadyTwo = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < h ; i++) {
-                    twoarr[i] = (float)( twoarr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                for (int i = 0; i < h; i++) {
+                    twoarr[i] = (float) (twoarr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
 
                 }
+
             }
         });
+
         myThreadyOne.start();
+        myThreadyOne.join();
         myThreadyTwo.start();
+        myThreadyTwo.join();
 
-        System.arraycopy(onearr,0,arr,0,h);
-        System.arraycopy(twoarr,0,arr,h,h);
 
-        System.out.println("Выполнение в двух потоках " + Double.toString(System.currentTimeMillis()-a));
+        System.arraycopy(onearr, 0, arr, 0, h);
+        System.arraycopy(twoarr, 0, arr, h, h);
+
+        System.out.println("Выполнение в двух потоках " + Double.toString(System.currentTimeMillis() - a));
 
     }
 }
