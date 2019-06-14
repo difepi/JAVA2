1cfb89ae561d2d086579268208cffca729bfdfb3
difepi <df@mymedtech.ru>
2019-06-14 14:37:28 +0300

    Домашнее задание 6. Консольный вариант клиент\серверного приложения.

---
+++ src/lesson6/Client.java
@@ -0,0 +1,74 @@
+package lesson6;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Client {
+
+
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("localhost", 8777)) {
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+            DataOutputStream dataOutputStream = new DataOutputStream (outputStream);
+            DataInputStream dataInputStream = new DataInputStream (inputStream);
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Клиент активен.");
+            readClient(dataInputStream);
+            writeClient(dataOutputStream, scanner);
+            closeClient(socket, inputStream, outputStream, scanner);
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void closeClient(Socket socket, InputStream inputStream, OutputStream outputStream, Scanner scanner) throws IOException {
+        inputStream.close();
+        outputStream.close();
+        socket.close();
+        scanner.close();
+    }
+
+
+    private static void writeClient(DataOutputStream dataOutputStream, Scanner scanner) throws IOException {
+        while (true) {
+            System.out.print("Отправить сообщение на Сервер:: ");
+            String message = scanner.nextLine();
+            if (message.equalsIgnoreCase("/end")) {
+                break;
+            }
+            dataOutputStream.writeUTF(message);
+        }
+    }
+
+    private static void readClient (DataInputStream dataInputStream) throws InterruptedException {
+       Thread thread = new Thread(new Runnable() {
+           @Override
+           public void run() {
+               while (true) {
+                   try {
+                       String message = dataInputStream.readUTF();
+                       Date date = new Date();
+                       System.out.println("SERVER [" + date + "] :: " + message);
+                   } catch (IOException e) {
+
+                       e.printStackTrace();
+                       System.out.println("Соедиение с Сервером разорвано.--> /end для закрытия Клиента.");
+                       break;
+                   }
+               }
+
+           }
+       });
+        thread.setDaemon(true);
+        thread.start();
+
+    }
+
+}
+
---
+++ src/lesson6/Server.java
@@ -0,0 +1,80 @@
+package lesson6;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Server {
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(8777)) {
+            System.out.println("Сервер " + serverSocket.isBound());
+            Socket socket = serverSocket.accept();
+            System.out.println(socket.toString() + "------" + socket.isConnected() );
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+            DataInputStream dataInputStream = new DataInputStream(inputStream);
+            DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+            Scanner scanner = new Scanner(System.in);
+
+            readServer(dataInputStream);
+
+            writeServer(dataOutputStream, scanner);
+
+            closeServer(serverSocket, socket, inputStream, outputStream, scanner);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void closeServer(ServerSocket serverSocket, Socket socket, InputStream inputStream,
+                                    OutputStream outputStream, Scanner scanner) throws IOException {
+        serverSocket.close();
+        socket.close();
+        inputStream.close();
+        outputStream.close();
+        scanner.close();
+    }
+
+    private static void writeServer(DataOutputStream dataOutputStream, Scanner scanner) throws IOException {
+        while (true) {
+            System.out.print("Отправить сообщение на Клиент:: ");
+            String message = scanner.nextLine();
+            if (message.equalsIgnoreCase("/end")){
+                break;
+            }
+            dataOutputStream.writeUTF(message);
+        }
+    }
+
+    private static void readServer(DataInputStream dataInputStream) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        String message = dataInputStream.readUTF();
+                        Date date = new Date();
+                        System.out.println("CLIENT [" + date + "] :: " + message);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        System.out.println("Соединение с клиентом разорвано. /end -- для закрытия Сервера. ");
+                        break;
+                    }
+                }
+            }
+        });
+        thread.setDaemon(true);
+        thread.start();
+    }
+
+
+}
+
+
+
+
